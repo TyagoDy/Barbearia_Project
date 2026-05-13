40d71726d7cbeee3a4fce37e8419f4cd0793bc35
Tyago_domingos@hotmail.com <Tyago_domingos@hotmail.com>
2026-05-13 19:02:54 -0300

    Folders in order

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Hello, vite! Pika</h1>
+        </header>
+    )
+}
---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,7 @@
+export default function footer() {
+    return (
+        <footer>
+            <p>© 2026 Vite. All rights reserved.</p>
+        </footer>
+    )
+}
