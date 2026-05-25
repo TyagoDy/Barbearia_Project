7492c92820e0b6819d62ea33e99a282313299fdc
Tyago_domingos@hotmail.com <Tyago_domingos@hotmail.com>
2026-05-25 19:55:31 -0300

    Front begining

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.database import Base, engine
 from app import routes
 from app.routes import barbers, clients, appointments
+from fastapi.middleware.cors import CORSMiddleware
+
 
 Base.metadata.create_all(bind=engine)
 
@@ -11,6 +13,14 @@ app.include_router(barbers.router)
 app.include_router(clients.router)
 app.include_router(appointments.router)
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
